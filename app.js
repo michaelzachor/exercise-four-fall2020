0246f77cfd61392788e364e70dc134dea8c1a6f9
Michael Zachor <mfz226@nyu.edu>
2020-11-05 15:39:19 -0500

    change POST to PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.POST || 4000;
+const port = process.env.PORT || 4000;
 
 const indexRoute = require(`./routes/index.js`);
 
