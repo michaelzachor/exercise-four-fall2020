65bcf5e6425b38f407b2d7177cedce468f19c13b
Michael Zachor <mfz226@nyu.edu>
2020-11-05 14:22:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.POST || 4000;
+
+const indexRoute = require(`./routes/index.js`);
+
+app.use('/', indexRoute);
+
+app.listen(port, () => 
+    console.log(`Exercise Four is running at localhost:${port}`)
+);
