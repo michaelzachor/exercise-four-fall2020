b7d925e3ea7336d1ae27f41ded364fe8f8508cbe
Michael Zachor <mfz226@nyu.edu>
2020-11-11 18:35:28 -0500

    Connect firebase

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-const indexRoute = require(`./routes/index.js`);
+// Configuration Values for Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCMffPETdMpU42H0eyRh5z-uc3q3YEesIw",
+    authDomain: "exercise-four-fall2020-5a6dc.firebaseapp.com",
+    databaseURL: "https://exercise-four-fall2020-5a6dc.firebaseio.com",
+    projectId: "exercise-four-fall2020-5a6dc",
+    storageBucket: "exercise-four-fall2020-5a6dc.appspot.com",
+    messagingSenderId: "496818769575",
+    appId: "1:496818769575:web:1459b715e2e7c608081582"
+};
 
+
+// Firebase
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+// Routes Import
+const indexRoute = require(`./routes/index.js`);
+const postRoute = require("./routes/post.js");
+const createRoute = require("./routes/createBlogpost.js");
 app.use('/', indexRoute);
+app.use('/post', postRoute);
+app.use('/create', createRoute);
 
 app.listen(port, () => 
     console.log(`Exercise Four is running at localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
+// Show all blogposts
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send(`Exercise Four`));
+// Require Firebase
+const firebase = require("firebase");
+
+// Initialize Firestore Database
+const db = firebase.firestore();
+
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+router.get('/', (req, res) => {
+    // res.send(`Show all blogposts`)
+    // We can do anything in here, just return at the end
+    const blogpostsArray = [];
+    blogposts
+    .get()
+    .then((querySnapshot) => {
+        // Loop through query snapshot and push into array
+        querySnapshot.forEach(doc => {
+            blogpostsArray.push(doc.data())
+        })
+        // return array
+        return res.send(blogpostsArray);
+    }).catch(function(e) {
+        console.warn('error:', e);
+        return res.send(e);
+    });
+});
 
 module.exports = router;
